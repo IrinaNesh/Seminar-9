a7d4a14a8c643a6aebd77727f80d66390b8c307d
Irina <ira.nesheret@yandex.ru>
2022-08-25 20:34:02 +0300

    Добавила решение задач

---
+++ 64/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 64: 
+Задайте значения M и N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8""
+*/
+Console.Write("Введите число -m- : ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число -n- : ");
+int n = int.Parse(Console.ReadLine());
+
+// Рекурсивный метод
+void NaturalNumberLineMN(int start, int stop)
+{
+    if (start > stop) return;
+
+    else
+    {
+        Console.Write(start + " ");
+        NaturalNumberLineMN(++start, stop);
+    }
+}
+// Обычный метод
+void NaturalNumberLineMN2(int start, int stop)
+{
+    for (int i = start; i <= stop; i++) Console.Write(i + " ");
+}
+
+Console.WriteLine("Рекурсивный метод:");
+NaturalNumberLineMN(m, n);
+
+Console.WriteLine();
+
+Console.WriteLine("Обычный метод:");
+NaturalNumberLineMN2(m, n);
---
+++ 68/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 68: 
+Напишите программу вычисления функции Аккермана 
+с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+Console.WriteLine("Введите значение -m- :");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение -n- :");
+int n = int.Parse(Console.ReadLine());
+
+
+int FunctionAkkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return FunctionAkkerman(m - 1, 1);
+    else return FunctionAkkerman(m - 1, FunctionAkkerman(m, n - 1));
+}
+
+Console.WriteLine("Функция Аккермана:" + $"\nA({m},{n}) = {FunctionAkkerman(m,n)}");
+
